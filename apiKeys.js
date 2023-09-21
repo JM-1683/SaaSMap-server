e4953cdcc0a96e84e2125458314ae70546c09134
JM-1683 <144040476+JM-1683@users.noreply.github.com>
2023-09-20 18:35:57 -0700

    Deleted apiKeys.js
    An old scratch-file with hard-coded keys was accidentally uploaded

--- apiKeys.js
+++
@@ -1,6 +0,0 @@
-const apiKey = 'NWI0YjkyN2MtYWExNy00ZGQ3LWJkNTMtYWM5NGE1Njk5ZGY4Om9xNnFiMGQwam5RWmhTWUI1VzVq'; //API key for SaaS alerts
-const Token = "pk.eyJ1Ijoiam1hcjEiLCJhIjoiY2xsbGx5ZGgzMXhhczNzcnI3cXdlYWQ4MiJ9._BTwBSGtGNexmu3_RREmuQ" //API key for MapboxGL
-
-module.exports = {
-    apiKey, Token
-}
